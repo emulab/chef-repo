3b0f64ba7fbaf12774b54e137fb2af4247a577eb
Dmitry Duplyakin <dmitry.duplyakin@colorado.edu>
2016-01-07 16:22:59 -0500

    Fix: skip install if HPGMG or PETSc already there
    - Also, don't run BLIS test - just print a log message about how to run it (similar to HPGMG)

--- cookbooks/emulab-hpgmg/recipes/default.rb
+++ cookbooks/emulab-hpgmg/recipes/default.rb
@@ -20,8 +20,10 @@ bash 'HPGMG: configure and make' do
     export PETSC_DIR=#{node['petsc']['root_level_dir']}/petsc PETSC_ARCH=build
     ./configure 2>&1 > .configure.log
     make HPGMG_CFLAGS='#{flags}' 2>&1 > .make.log
+    touch .completed   
   EOH
   cwd "#{node['hpgmg']['root_level_dir']}/hpgmg"
+  not_if { File.exists?("#{node['hpgmg']['root_level_dir']}/hpgmg/.completed") }
 end
 
 log "To run HPGMG, try, for instance: mpiexec -n 4 --allow-run-as-root #{node['hpgmg']['root_level_dir']}/hpgmg/build/bin/hpgmg-fe sample -local 1e3,2e3 -op_type poisson2 -log_summary"
--- cookbooks/emulab-petsc/recipes/default.rb
+++ cookbooks/emulab-petsc/recipes/default.rb
@@ -17,6 +17,8 @@ bash 'PETSC: configure, make, install' do
     ./configure --with-debugging=0 --prefix="#{node['petsc']['root_level_dir']}/petsc" --download-f2cblaslapack=1 --with-shared-libraries=0 2>&1 > .configure.log 
     make MAKE_NP=#{node["cpu"]["total"]} all 2>&1 > .make.all.log
     make install 2>&1 > .make.install.log
+    touch "#{node['petsc']['root_level_dir']}/petsc/.completed"
   EOH
   cwd "#{node['petsc']['root_level_dir']}/petsc-src"
+  not_if { File.exists?("#{node['petsc']['root_level_dir']}/petsc/.completed") }
 end
