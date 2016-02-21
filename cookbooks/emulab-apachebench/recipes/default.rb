a05c317df0f0559464579a20ccdf62cc972cd9ef
Dmitry Duplyakin <dmitry.duplyakin@colorado.edu>
2016-02-20 18:39:58 -0700

    Apache2 and apachebench
    - Add role for apache2 and new cookbook for apachebench

---
+++ cookbooks/emulab-apachebench/recipes/default.rb
@@ -0,0 +1,69 @@
+#
+# Cookbook Name:: emulab-apachebench
+# Recipe:: default
+#
+
+# Install the package with the benchmark 
+package "apache2-utils" 
+# Install gnuplot for plotting benchmark output
+package "gnuplot"
+
+# If the target_host attribute is not set (e.g., from a role), benchmark local host 
+if node["apachebench"]["target_host"].nil?
+  target = node["fqdn"]
+else
+  target = node["apachebench"]["target_host"]
+end
+
+log "Benchmarking #{target}"
+
+# Default values for these attributes are specified inside attributes/default.rb
+ports = node["apachebench"]["ports"]
+n = node["apachebench"]["request_number"]
+c = node["apachebench"]["request_concurrent"]
+out = node["apachebench"]["output"]
+
+ports.each do |port|
+  if port == 443
+    url = "https://#{target}:#{port}/"
+  else
+    url = "http://#{target}:#{port}/"
+  end
+
+  output_file = "#{out}.#{port}"
+  # To avoid possible confusion, delete the version of the output file if it exists
+  file output_file do
+    action :delete
+  end
+    
+  # Run the benchmark (via calling a shell command)
+  execute "ab -n #{n} -c #{c} -g #{output_file} #{url}" do
+    ignore_failure true
+  end
+
+  plot_script = "#{output_file}-plot"
+  graph_file = "#{output_file}.png" 
+
+  # Create a gnuplot script for processing this specific output (using a template)
+  template plot_script do
+    source "apachebench-plot"
+    variables(
+      :input  => output_file,
+      :output  => graph_file )
+  end
+
+  # Plot the output using the created script
+  execute "gnuplot #{plot_script}" do
+    ignore_failure true
+  end
+  
+  # Check that apache is installed (via checking the attribute) and the docroot_dir is set 
+  if (not node["apache"].nil?) && (not node["apache"]["docroot_dir"].nil?)  
+    # Copy the created graph to the apache's document root
+    remote_file "Publish the graph" do 
+      path "#{node['apache']['docroot_dir']}/#{target}-#{port}.png" 
+      source "file://#{graph_file}"
+      ignore_failure true
+    end
+  end
+end
---
+++ roles/apache2.rb
@@ -0,0 +1,16 @@
+#
+# This role depends on the apache2 cookbook
+# available at: https://supermarket.chef.io/cookbooks/apache2
+# Make sure it is installed. If it is not installed, try: 
+# knife cookbook site install apache2
+#
+name "apache2"
+description "This role is applied to the nodes on which apache2 should be installed"
+override_attributes(
+  "apache" => {
+    "listen_ports" => [8080],
+    "default_site_port" => "8080",
+    "default_site_enabled" => true
+  }
+)
+run_list [ "apache2", "apache2::mod_autoindex" ]
