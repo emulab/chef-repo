3fde6a4a8f3a811190c108a427d9a26c2e06ab54
Dmitry Duplyakin <dmitry.duplyakin@colorado.edu>
2016-11-01 13:28:21 -0500

    emulab-powervis: cleanup

--- cookbooks/emulab-powervis/recipes/default.rb
+++ cookbooks/emulab-powervis/recipes/default.rb
@@ -42,16 +42,21 @@ git '/srv/shiny-server' do
 end
 
 # Optional: add a sample of power data in /var/log/power so the dashboard doesn't return an error
-directory "/var/log/power"
-template "/var/log/power/SAMPLE.csv" do
+directory node['powervis']['logs_dir'] do
+  mode '0755'
+end
+
+template "#{node['powervis']['logs_dir']}/random_sample.csv" do
   source 'SAMPLE.erb'
   mode 0644
+  # Only add this sample if the dir is empty (two entries are "." and "..")
+  only_if { ::Dir.entries(node['powervis']['logs_dir']).size <= 2 }
 end
 
 # Integration with apache2 for publishing data sets
 
 # Install apache2 using apache2 cookbook
-node.override["apache"]["listen_ports"]= %w(8080)
+node.override["apache"]["listen"]= %w(8080)
 node.override["apache"]['default_site_port']= "8080"
 node.override['apache']['default_site_enabled']= true
 node.override['apache']['docroot_dir']= "/var/www"
